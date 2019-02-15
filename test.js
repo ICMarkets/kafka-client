20c16f55fbabb0ee5dc18f5974c9744238ea1437
Vladimir Spirin <spirin.vladimir@gmail.com>
2019-02-15 08:37:39 +0200

    add deps: nyc mocha coveralls

--- test.js
+++ test.js
@@ -3,9 +3,12 @@ var kafka = require('./index')(kafkaHost);
 var topic   = 'test-topic-encoded-with-messagepack-print-pow2' + Math.random();
 var pow2 = 1;
 
-kafka.send(topic, pow2, () =>
-    kafka.stream(topic).onValue(value => {
-        console.log(value);
-        pow2 += value;
-        setTimeout(() => kafka.send(topic, pow2), 1000);
-    }))
+describe('pub/sub', () =>
+    it('send 1 to topic & get 1 from topic', () =>
+        kafka.send(topic, pow2, () =>
+            kafka.stream(topic).onValue(value =>
+                done(
+                    pow === value
+                        ? undefined
+                        : value
+                    )))))
