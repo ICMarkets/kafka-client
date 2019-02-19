ab024d3485c432fad13f10b2e2c97e10c5a21a05
Vladimir Spirin <spirin.vladimir@gmail.com>
2019-02-19 14:34:50 +0200

    fix test & new API : send_key, stream_group

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ function send (topic, message, done) {
     else create_producer(() => send(topic, message, done))
 }
 
+function send_key (topic, key, message, done) {
+    if (producer) producer.send([{topic, key, messages: encode(message)}], done || skip)
+    else create_producer(() => send(topic, message, done))
+}
+
 function stream (topic) {
     return signal(emit => {
         var client = new Kafka.KafkaClient({kafkaHost})
@@ -34,6 +39,30 @@ function stream (topic) {
                     {
                         fromOffset: true,
                         groupId: String(Date.now()) + Math.random(),
+                        encoding: 'buffer'
+                    }
+                )
+                consumer.on('error',  on_error)
+                consumer.on('message', message => emit(decode(message.value)))
+            })
+        )
+    })
+}
+
+function stream_group (topic, groupId) {
+    return signal(emit => {
+        var client = new Kafka.KafkaClient({kafkaHost})
+        client.on('error', on_error)
+        client.on('ready', () =>
+            new Kafka.Offset(client).fetch([{ topic, time: -1}], (error, offsets_per_partitions) => {
+                if (error) on_error(error)
+                var offset = offsets_per_partitions[topic]['0'][0] - 1
+                var consumer = new Kafka.Consumer(
+                    client,
+                    [{topic, offset}],
+                    {
+                        fromOffset: true,
+                        groupId,
                         encoding: 'buffer'
                     }
                 )
@@ -71,5 +100,11 @@ function filter (topic, key) {
 module.exports = _ => {
     kafkaHost = _;
 
-    return {send, stream, filter};
+    return {
+        send,
+        send_key,
+        stream,
+        stream_group,
+        filter
+    };
 }
--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 var kafkaHost = 'kafka.devicm:9092';
 var kafka = require('./index')(kafkaHost);
 var topic   = 'test-topic-encoded-with-messagepack-print-pow2' + Math.random();
-var pow2 = 1;
+var message = 1;
 
 describe('pub/sub', () =>
     it('send 1 to topic & get 1 from topic', () =>
-        kafka.send(topic, pow2, () =>
+        kafka.send(topic, message, () =>
             kafka.stream(topic).onValue(value =>
                 done(
-                    pow === value
+                    message === value
                         ? undefined
                         : value
                     )))))
